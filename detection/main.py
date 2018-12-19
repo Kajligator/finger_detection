aa37124fc37d92f8e829e0bbe24fc3456eae1cc0
BuildTools <unconfigured@null.spigotmc.org>
2018-12-19 17:10:42 +0100

    Added contours

--- detection/main.py
+++ detection/main.py
@@ -12,30 +12,32 @@ MAX_HSV = np.array([29, 114, 255])
 element_size = 5
 
 camera = cv.VideoCapture(0)
-camera.set(10,200)
-# cv.namedWindow('trackbar')
-# cv.createTrackbar('trh1', 'trackbar', THRESHOLD, 100, printThreshold)
+camera.set(10, 200)
 
+kernel = np.ones((3, 3), np.uint8)
 
 while camera.isOpened():
     _, frame = camera.read()
 
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    mask = cv.inRange(hsv, MIN_HSV, MAX_HSV)
+    dilation = cv.dilate(hsv, kernel, iterations=1)
+
+    mask = cv.inRange(dilation, MIN_HSV, MAX_HSV)
 
     res = cv.bitwise_and(frame, frame, mask=mask)
 
     blur = cv.medianBlur(res, 5)
-    kernel = np.ones((3, 3), np.uint8)
 
     element = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2 * element_size + 1, 2 * element_size + 1), (element_size, element_size))
-    dilation = cv.dilate(blur, kernel, iterations=1)
     cv.imshow('frame', frame)
     cv.imshow('mask', mask)
     cv.imshow('res', dilation)
 
-    # cnt = cv.findContours(dilation, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE, (0, 0))
+    _, contours = cv.findContours(blur, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     largest_contour = 0
+
+    for cnt in contours:
+        print(cnt)
     # for i in len(cnt):
     # print(cnt)
     k = cv.waitKey(10)
