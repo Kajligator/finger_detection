5435b54ee17706069073cb0bf8cece0fab757cca
Kaj van der Ster <kaj.vd.ster@gmail.com>
2019-01-06 12:36:38 +0100

    Changed: the hsv values, the dilation

--- detection/main.py
+++ detection/main.py
@@ -17,8 +17,8 @@ FONT_THICKNESS = 3
 
 # MIN_HSV = np.array([0, 0, 221])
 # MAX_HSV = np.array([29, 114, 255])
-MIN_HSV = np.array([0, 0, 160])
-MAX_HSV = np.array([179, 88, 255])
+MIN_HSV = np.array([0, 0, 255])
+MAX_HSV = np.array([179, 45, 255])
 
 camera = cv.VideoCapture(0)
 camera.set(10, 200)
@@ -59,6 +59,8 @@ def calculate_inner_angle(px1, py1, px2, py2, cx1, cy1):
     dist1 = math.sqrt((px1 - cx1) * (px1 - cx1) + (py1 - cy1) * (py1 - cy1))
     dist2 = math.sqrt((px2 - cx1) * (px2 - cx1) + (py2 - cy1) * (py2 - cy1))
 
+    # print("d1: {}\n"
+    #       "d2: {}".format(dist1, dist2))
     cx = cx1
     cy = cy1
     if dist1 < dist2:
@@ -73,13 +75,13 @@ def calculate_inner_angle(px1, py1, px2, py2, cx1, cy1):
         p2 = py2 - py1
 
     awns = math.acos((p1 * q1 + p2 * q2) / (math.sqrt(p1 * p1 + p2 * p2) * math.sqrt(q1 * q1 + q2 * q2))) * 180 / math.pi
+    print("A: {}".format(awns))
     return awns
 
 
 while camera.isOpened():
     _, frame = camera.read()
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    dilation = cv.dilate(hsv, kernel, iterations=1)
+
 
     # read trackbar positions for all
     hul = cv.getTrackbarPos(hl, barsWindow)
@@ -92,9 +94,12 @@ while camera.isOpened():
     # make array for final values
     HSVLOW = np.array([hul, sal, val])
     HSVHIGH = np.array([huh, sah, vah])
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-    mask = cv.inRange(dilation, HSVLOW, HSVHIGH)
-    ret, thresh = cv.threshold(mask, THRESHOLD, 255, cv.THRESH_BINARY)
+    mask = cv.inRange(hsv, HSVLOW, HSVHIGH)
+    dilation = cv.dilate(mask, kernel, iterations=1)
+
+    ret, thresh = cv.threshold(dilation, THRESHOLD, 255, cv.THRESH_BINARY)
 
     # Check for contours and pick the largest one
     _, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -131,20 +136,22 @@ while camera.isOpened():
                     point1 = tuple(res[s][0])
                     point2 = tuple(res[e][0])
                     point3 = tuple(res[f][0])
+                    # cv.circle(frame, point3, 10, (0, 255, 0), -1)
                     angle = math.atan2(center_y - point1[1], center_x - point1[0]) * 180 / math.pi
                     inAngle = calculate_inner_angle(point1[0], point1[1], point2[0], point2[1], point3[0], point3[1])
                     length = math.sqrt(math.pow(point1[0] - point3[0], 2) + math.pow(point1[0] - point3[1], 2))
-                    if not (not (angle > -30) or not (angle < 160) or not (20 < math.fabs(inAngle) < 120)) and length > 0.1 * h:
-                    # if not (not (angle > -30) or not (angle < 180) or not (25 < math.fabs(inAngle) < 120)) and length > 0.18 * h:
-
+                    # if not (not (angle > -30) or not (angle < 160) or not (20 < math.fabs(inAngle) < 120)) and length > 0.1 * h:
+                    if not (not (angle > -160) or not (angle < 160) or not (25 < math.fabs(inAngle) < 120)) and length > 0.18 * h:
                         valid_points.append(point1)
+                        # valid_points.append(point2)
+                    #     print("p1: {} p2 {}".format(point1, point2))
 
                     # cv.line(frame, point1, point3, (64, 255, 64), 3)
                     # cv.line(frame, point3, point2, (255, 128, 64), 3)
                 length_of_valid_points = len(valid_points)
                 for i in range(length_of_valid_points):
                     cv.circle(frame, valid_points[i], 10, (255, 0, 0), 3)
-                    print(length_of_valid_points)
+                    # print(length_of_valid_points)
                     cv.putText(frame, str(length_of_valid_points), (10, 200), FONT_FACE, FONT_SCALE, (255, 128, 0), FONT_THICKNESS, cv.LINE_AA)
 
     cv.imshow('frame', frame)
